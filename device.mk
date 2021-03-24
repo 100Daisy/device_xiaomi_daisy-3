492fc6d81fe7537ad4d0ed5c4c461e811ac93661
100Daisy <100Daisy@protonmail.com>
2021-03-24 09:29:58 +0100

    GAPPS: Debloat

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# Debloat GAPPS
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
