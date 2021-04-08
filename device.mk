112c7c5d1f55928791b41ee54e9a282002464326
100Daisy <100Daisy@protonmail.com>
2021-04-08 09:32:01 +0200

    debloat gapps

--- device.mk
+++ device.mk
@@ -47,6 +47,13 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIM_LOW_RES := true
 
+# GAPPS
+ifeq ($(WITH_GAPPS), true)
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+endif
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
