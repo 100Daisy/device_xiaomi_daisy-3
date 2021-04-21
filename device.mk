e258a1a3c3d9b96eb65e9be0777b4716d273826a
100Daisy <48625180+100Daisy@users.noreply.github.com>
2021-04-21 15:52:51 +0200

    omx

--- device.mk
+++ device.mk
@@ -363,6 +363,8 @@ PRODUCT_COPY_FILES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service \
     libc2dcolorconvert \
     libmm-omxcore \
     libOmxAacEnc \
